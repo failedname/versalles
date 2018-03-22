fb6132abf578f226373c5b4897ab487b64ad6bbe
javier nuñez <javier@MacBook-Pro-de-javier.local>
2018-03-22 11:20:06 -0500

    v

--- versalles/settings/production.py
+++ versalles/settings/production.py
@@ -11,7 +11,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'versalles',
-        'USER': 'app_vivero',
+        'USER': 'javier',
         'PASSWORD': 'luna',
         'HOST': 'localhost',
         'PORT': '5432',
